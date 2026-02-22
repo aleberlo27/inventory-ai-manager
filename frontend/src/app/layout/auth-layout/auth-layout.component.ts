c6f74c770338785c2c3f53d2bfcdaf20b82f07d7
Alejandra Bernal López <alejandrabernallopez1@gmail.com>
2026-02-22 22:26:48 +0100

    test: add AuthLayout component with TDD

--- frontend/src/app/layout/auth-layout/auth-layout.component.ts
+++ frontend/src/app/layout/auth-layout/auth-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatePipe],
   templateUrl: 'auth-layout.component.html',
 })
 export class AuthLayoutComponent {}
